8f12a58c405fe399e1d8c3965fc73921c4427c1d
gustavo cordoba <cordobagustavoj@gmail.com>
2021-10-28 16:53:48 -0300

    generar JSON

--- TP3/src/archivos/ManejoDeArchivos.java
+++ TP3/src/archivos/ManejoDeArchivos.java
@@ -1,31 +1,24 @@
 package archivos;
 
 import java.io.BufferedReader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 
 public class ManejoDeArchivos {
-	
-	
-	static void muestraContenido(String archivo) throws FileNotFoundException, IOException {
-		try {  
-		FileInputStream f = new FileInputStream(archivo);
-	      Scanner scanner = new Scanner(f);
-		
-	      while( scanner.hasNextLine() )
-	    	  System.out.println( scanner.nextLine() );
-		}
-		
-	 catch(Exception e) {
-	       e.printStackTrace();
-	       }
-}
 	
 	
 	static String miDirectorio() {
@@ -43,8 +36,35 @@ public class ManejoDeArchivos {
 	
 	
 	
+	
+	private String generarJSONPretty()
+	{
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(this);
+		
+		return json;
+	}
+	
+	public void generarJSON(String archivo)
+	{
+		String json = this.generarJSONPretty();
+		
+		try{
+			FileWriter writer = new FileWriter(archivo);
+			writer.write(json);
+			writer.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	
+	
 	   public static void main(String[] args) throws FileNotFoundException, IOException  {
-		   muestraContenido(miDirectorio());
 		  
 		     }	
 	
