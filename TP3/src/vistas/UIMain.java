9e8a2ca730dbbd4836f41d4a5c756fa748f8c3f5
gustavo cordoba <cordobagustavoj@gmail.com>
2021-10-29 17:49:45 -0300

    se agrego boton para asignar arbitros

--- TP3/src/vistas/UIMain.java
+++ TP3/src/vistas/UIMain.java
@@ -1,12 +1,17 @@
 package vistas;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class UIMain {
 	
 	private Fondo fondo;
-	private JButton asignarArbitros;
+	private JButton BtnAsignarArbitros;
 	private JFrame frameMenuPrincipal;
 
 	
@@ -29,8 +34,31 @@ public class UIMain {
 		frameMenuPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameMenuPrincipal.setTitle("Torneo de football" );
 		frameMenuPrincipal.setVisible(true);
-
+		
+		
+		//inicializo el boton 
+		
+		/*------------Boton para ver calendario--------------------*/
+		BtnAsignarArbitros= new JButton("asignar arbitros");
+		BtnAsignarArbitros.setBackground(new Color(0, 0, 0));
+		BtnAsignarArbitros.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		BtnAsignarArbitros.setBounds(0,0, 300, 80);
+		BtnAsignarArbitros.setLocation((frameMenuPrincipal.getWidth()/2-BtnAsignarArbitros.getWidth()/2),
+									(frameMenuPrincipal.getHeight()/2 + BtnAsignarArbitros.getHeight()/2) + 100 );
+		
+		//BtnAsignarArbitros.addActionListener(new ActionListener() {
+		
+			//public void actionPerformed(ActionEvent e) {
+			
+	// aca se va a generar la asignacion desde la vista 
+			//	}
+			//});
+		
+		frameMenuPrincipal.getContentPane().add(BtnAsignarArbitros);
 		
 	}
+	
+	
+	
 
 }
