d0e9bb5dd1b674844ce3dc72ca3b059fd95ca025
gustavo cordoba <cordobagustavoj@gmail.com>
2021-10-29 18:03:06 -0300

    ventana para el calendario

--- TP3/src/vistas/ventanaTorneo.java
+++ TP3/src/vistas/ventanaTorneo.java
@@ -1,4 +1,9 @@
 package vistas;
+ 
+
+//aca te traes todo el controlador (main controller) y lo vas agregando al jpanel
+// despues de eso en el main vas a agregar esta ventana
+
 
 public class ventanaTorneo {
 
